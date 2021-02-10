7c31311a31b13fdf5ebcac0f6f2d7f94404ccb50
Rafael Carvalho Monteiro de Morais <rafael@qwkin.io>
2021-02-10 15:49:58 -0300

    feat: created torch control interface

--- packages/torch_control/lib/torch_control.dart
+++ packages/torch_control/lib/torch_control.dart
@@ -1,18 +1,92 @@
-// You have generated a new plugin project without
-// specifying the `--platforms` flag. A plugin project supports no platforms is generated.
-// To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
-// directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
-
 import 'dart:async';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class TorchControl {
-  static const MethodChannel _channel =
-      const MethodChannel('torch_control');
+  final _channel = const MethodChannel('torch_control');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  bool _ensureInitialized = false;
+  double _torchIntensity;
+  bool _debug;
+  bool _isTorchOn;
+  bool _suppressTorchErrors;
+
+  double get torchIntensity => _torchIntensity;
+  bool get isTorchOn => _isTorchOn;
+
+  static final TorchControl _singleton = TorchControl._internal();
+  factory TorchControl() {
+    return _singleton;
+  }
+  TorchControl._internal();
+
+  void initialize(
+      {double intensity = 1.0,
+      bool torchEnabledWhenInitialize = false,
+      bool debug = false,
+      bool suppressTorchErrors = false}) async {
+    _ensureInitialized = true;
+    _torchIntensity = intensity;
+    _suppressTorchErrors = suppressTorchErrors;
+
+    if (torchEnabledWhenInitialize) {
+      _isTorchOn = true;
+      on(intensity: _torchIntensity);
+    } else {
+      _isTorchOn = false;
+      off();
+    }
+  }
+
+  Future<bool> get hasTorch async => await _channel.invokeMethod('hasTorch');
+
+  Future<void> on({double intensity}) async {
+    assert(Platform.isAndroid && intensity != null,
+        "You can only control torch intensity on iOS");
+    assert(_ensureInitialized, '''
+      You must initialize your `TorchControl` instance on your runApp().
+      You can perform this by adding the following line to your main.dart:
+      
+      `TorchControl.initialize();`
+    ''');
+
+    if (intensity != null) {
+      _torchIntensity = intensity;
+    }
+
+    try {
+      await _channel.invokeMethod('turnOn', [_torchIntensity]);
+    } catch (error) {
+      if (_debug) {
+        debugPrint(error);
+      }
+
+      if (!_suppressTorchErrors) {
+        rethrow;
+      }
+    }
+  }
+
+  Future<void> off({double intensity}) async {
+    assert(_ensureInitialized, '''
+      You must initialize your `TorchControl` instance on your runApp().
+      You can perform this by adding the following line to your main.dart:
+      
+      `TorchControl.initialize();`
+    ''');
+
+    try {
+      await _channel.invokeMethod('turnOff');
+    } catch (error) {
+      if (_debug) {
+        debugPrint(error);
+      }
+
+      if (!_suppressTorchErrors) {
+        rethrow;
+      }
+    }
   }
 }
