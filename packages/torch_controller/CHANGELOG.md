2553309e5bb7db594ad598e28c15879c705122aa
Rafael Carvalho Monteiro de Morais <rafael@qwkin.io>
2021-02-10 18:08:52 -0300

    chore: update class docs

--- packages/torch_controller/CHANGELOG.md
+++ packages/torch_controller/CHANGELOG.md
@@ -1,8 +1,12 @@
+## 0.1.1
+- Added docs for `torch_controller` methods
+- Created a `README.md` for `torch_controller`
+
 ## 0.1.0
 - Added `torch_controller` package
 - Added `torch_controller` iOS support
 - Created `BSD License`
-- Created `CHANGELOG.md`g
+- Created `CHANGELOG.md`
 - Created tests for `torch_controller` methods
 
 ## 0.0.1
--- packages/torch_controller/lib/torch_controller.dart
+++ packages/torch_controller/lib/torch_controller.dart
@@ -4,7 +4,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+/// The `TorchController` class presents a global controller to use native torch/flash resources
 class TorchController {
+  /// Private method channel name
   final _channel = const MethodChannel('torch_control');
 
   bool _ensureInitialized = false;
@@ -12,14 +14,27 @@ class TorchController {
   bool _debug;
   bool _suppressTorchErrors;
 
+  /// Get current active torch intensity. Only relevant on `iOS`
   double get torchIntensity => _torchIntensity;
 
+  /// Singleton implementation.
+  ///
+  /// Grants the singleton behavior using this package through app lifecycle
   static final TorchController _singleton = TorchController._internal();
   factory TorchController() {
     return _singleton;
   }
   TorchController._internal();
 
+  /// Initialize configurations that you want to initialize your controller.
+  ///
+  /// Must be runner on `main.dart` before call `runApp`. It will ensure that the
+  /// controller instance will be available within all your app
+  ///
+  /// - double intensity: Sets the default intensity of the torch. Only relevant on iOS
+  /// - debug: Enable/disable debugPrint of error thrown by the methods
+  /// - suppressTorchErrors: Suppress native errors from the usage of the torch in different devices. Active this if you
+  ///   don't want or need to handle torch errors
   void initialize(
       {double intensity = 1.0,
       bool debug = false,
@@ -30,11 +45,27 @@ class TorchController {
     _suppressTorchErrors = suppressTorchErrors;
   }
 
+  /// Return a `bool` if the current device is able to use torch methods.
+  ///
+  /// You can use this to display something in your app configuration to handle torch usage.
   Future<bool> get hasTorch async => await _channel.invokeMethod('hasTorch');
 
+  /// Return a `bool` that checks the torch state nativelly.
+  ///
+  /// You can use this to enable/disable a toggle in it's initial state.
   Future<bool> get isTorchActive async =>
       await _channel.invokeMethod('isTorchActive');
 
+  /// Execute the action to toggle torch on/off. Returns the current torch state.
+  ///
+  /// double intensity: Define the desired intensity and set as default
+  ///
+  /// Example:
+  ///
+  /// final controller = TorchController();
+  ///
+  /// controller.toggle() // Activates the torch and returns true;
+  /// controller.toggle() // Deactivates the torch and returns false;
   Future<bool> toggle({double intensity}) async {
     if (Platform.isAndroid && intensity != null) {
       throw (AssertionError("You can only control torch intensity on iOS"));
