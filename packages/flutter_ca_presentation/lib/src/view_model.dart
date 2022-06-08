b33e5ee79892f350fef462c26d519d9d275f09fb
Rafael Carvalho Monteiro <contato.rdev@gmail.com>
2022-06-08 11:04:36 -0300

    feat: initial commit main module

--- packages/flutter_ca_presentation/lib/src/view_model.dart
+++ packages/flutter_ca_presentation/lib/src/view_model.dart
@@ -2,6 +2,17 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_ca_domain/flutter_ca_domain.dart';
 
 abstract class ViewModel extends Disposable with ChangeNotifier {
+  bool _busy = false;
+  bool get busy => _busy;
+
+  void _setBusy(bool value) {
+    _busy = value;
+    notifyListeners();
+  }
+
+  void start() => _setBusy(true);
+  void end() => _setBusy(false);
+
   @mustCallSuper
   ViewModel() {
     initListeners();
