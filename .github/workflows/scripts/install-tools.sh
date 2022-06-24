58cc604ea6b854a87f0c310a16431eafce89c6e3
João Pereira <joaocarlospereira.jp@gmail.com>
2022-06-24 10:40:48 -0300

    fix: update scripts with flutter prefix

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -2,6 +2,6 @@
 
 echo $GITHUB_PATH
 
-dart pub global activate melos
-dart pub global activate tuneup
-dart pub global activate flutter_plugin_tools
+dart flutter pub global activate melos
+dart flutter pub global activate tuneup
+dart flutter pub global activate flutter_plugin_tools
